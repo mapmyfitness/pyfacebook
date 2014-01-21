e67a81bcb0c7862c04309a57b0c8f642ecfb1d35
Dave McLain <dave.mclain@mapmyfitnessinc.com>
2014-01-20 18:06:14 -0600

    CORE-472 integrate release tools

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import sys
+
+if 'sdist' in sys.argv:
+    import mmf_release_tools
+    version = mmf_release_tools.generate_release_version("0.1", __file__)
+    mmf_release_tools.write_release_version(version)
+else:
+    with open("RELEASE-VERSION", "r") as f:
+        version = f.readlines()[0].strip()
+
 
 setup(name='pyfacebook',
-      version='0.1',
+      version=version,
       description='Python Client Library for the Facebook API',
       author='Samuel Cormier-Iijima',
       author_email='sciyoshi@gmail.com',
